7ede143b04d26b790f8db56387575eef6dbf7313
Dylan Hearn <dylanhearn123@gmail.com>
2024-02-25 17:42:03 +0000

    fix: alt tag for logo navbar

--- header.php
+++ header.php
@@ -9,6 +9,7 @@
     <meta name="description"
         content="Hello Chef | Explore our selection of hand-picked meals delivered straight to your door.">
     <meta name="author" content="Dylan Hearn">
+    <title>Hello Chef</title>
     <link rel="shortcut icon" href="images/logo.png">
     <link rel="icon" type="image/svg+xml" href="<?php echo get_template_directory_uri() . "/images/logo.svg" ?>" />
 
@@ -26,7 +27,8 @@
         <nav class="navbar navbar-expand-lg bg-body-tertiary" id="navbar">
             <div class="container-fluid w-75">
                 <a href="/">
-                    <img src="<?php echo get_template_directory_uri() . "/images/logonew.png" ?>" style="width: 90px">
+                    <img src="<?php echo get_template_directory_uri() . "/images/logonew.png" ?>" style="width: 90px"
+                        alt="hello chef logo">
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -34,17 +36,19 @@
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav">
-                        <?php
+                        <li>
+                            <?php
 
-                        wp_nav_menu(
-                            array(
-                                'topnav' => 'top navbar links',
-                                'container' => '',
-                                'theme_location' => 'top-nav',
-                            )
-                        );
+                            wp_nav_menu(
+                                array(
+                                    'topnav' => 'top navbar links',
+                                    'container' => '',
+                                    'theme_location' => 'top-nav',
+                                )
+                            );
 
-                        ?>
+                            ?>
+                        </li>
                     </ul>
                 </div>
             </div>
